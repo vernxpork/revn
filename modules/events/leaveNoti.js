b4b279e3b3eff2360f9004df376c06228bd91a89
vraxyxx <ryantayo57@gmail.com>
2025-06-30 08:45:42 +0800

    Update leaveNoti.js

--- modules/events/leaveNoti.js
+++ modules/events/leaveNoti.js
@@ -16,7 +16,7 @@ module.exports = {
         logMessageData.leftParticipantFbId === api.getCurrentUserID()
       ) {
         api.changeNickname(
-          `[ ${prefix} ]: NashBoT`,
+          `[ ${prefix} ]: 𝐑𝐞𝐯𝐧`,
           threadID,
           api.getCurrentUserID()
         );
